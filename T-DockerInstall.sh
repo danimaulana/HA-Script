797cbcd182030d4227f75e3317196aea3fcb16c0
Dani Maulana Ferdiansyah <47852543+danimaulana@users.noreply.github.com>
2024-04-01 04:02:40 +0700

    Update T-DockerInstall.sh

--- T-DockerInstall.sh
+++ T-DockerInstall.sh
@@ -8,8 +8,8 @@ sudo apt update
 sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
 
 # Add Docker's official GPG key:
-sudo apt-get update
-sudo apt-get install ca-certificates curl
+sudo apt-get update -y
+sudo apt-get install ca-certificates curl -y
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -24,8 +24,8 @@ sudo apt-get update
 #Install the Docker packages.
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-sudo systemctl status docker
-
 sudo usermod -aG docker ubuntu
 
 docker run hello-world
+
+sudo systemctl status docker
