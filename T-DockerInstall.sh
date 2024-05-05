2f88b7fadb382a912960e9aeb295f1529ba3c75e
Dani Maulana Ferdiansyah <47852543+danimaulana@users.noreply.github.com>
2024-05-05 13:48:26 +0700

    Update T-DockerInstall.sh

--- T-DockerInstall.sh
+++ T-DockerInstall.sh
@@ -22,7 +22,7 @@ echo \
 sudo apt-get update
 
 #Install the Docker packages.
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 sudo usermod -aG docker ubuntu
 
