aa2416d3532e1cd83dea90cb4f3bd15cbc0e452c
Dani Maulana Ferdiansyah <47852543+danimaulana@users.noreply.github.com>
2024-05-30 05:38:34 +0700

    Update 2.init_swarm.sh

--- Docker-Swarm/2.init_swarm.sh
+++ Docker-Swarm/2.init_swarm.sh
@@ -1,26 +1,62 @@
 #!/bin/bash
 
-# List of IP addresses for master and worker nodes
+# Daftar alamat IP untuk node master dan worker
 MASTER_IPS=("192.168.122.61" "192.168.122.62" "192.168.122.63")
 WORKER_IPS=("192.168.122.64" "192.168.122.65")
 
-# SSH password for authentication
+# Kata sandi untuk autentikasi SSH dan sudo
 SSH_PASSWORD="ubuntu"
+SUDO_PASSWORD="ubuntu"
 
-# Initialize Swarm on the first master node
-docker swarm init --advertise-addr ${MASTER_IPS[0]}
+# Periksa dan instal sshpass jika belum ada
+if ! command -v sshpass &> /dev/null; then
+    echo "sshpass not found, installing..."
+    sudo apt-get update
+    sudo apt-get install -y sshpass
+fi
 
-# Get token for joining the Swarm as additional master nodes
-JOIN_TOKEN=$(docker swarm join-token -q worker)
+# Fungsi untuk memeriksa apakah node sudah menjadi bagian dari Swarm
+check_swarm_status() {
+    if sudo docker info | grep -q "Swarm: active"; then
+        echo "Node is already part of a swarm."
+        return 1
+    else
+        return 0
+    fi
+}
 
-# Join additional master nodes to the Swarm
+# Inisialisasi Swarm pada node master pertama jika belum menjadi bagian dari Swarm
+if check_swarm_status; then
+    echo $SUDO_PASSWORD | sudo -S docker swarm init --advertise-addr ${MASTER_IPS[0]}
+else
+    echo "Node ${MASTER_IPS[0]} is already part of a swarm. Skipping initialization."
+fi
+
+# Mendapatkan token untuk bergabung dengan Swarm sebagai manager dan worker
+MANAGER_JOIN_TOKEN=$(echo $SUDO_PASSWORD | sudo -S docker swarm join-token -q manager)
+WORKER_JOIN_TOKEN=$(echo $SUDO_PASSWORD | sudo -S docker swarm join-token -q worker)
+
+# Tampilkan token untuk bergabung dengan Swarm
+echo "Manager join token: $MANAGER_JOIN_TOKEN"
+echo "Worker join token: $WORKER_JOIN_TOKEN"
+
+# Fungsi untuk bergabungkan node ke Swarm
+join_swarm() {
+    local node_ip=$1
+    local token=$2
+    sshpass -p "$SSH_PASSWORD" ssh -o StrictHostKeyChecking=no ubuntu@$node_ip "echo $SUDO_PASSWORD | sudo -S docker swarm join --token $token ${MASTER_IPS[0]}:2377"
+}
+
+# Bergabungkan node master tambahan ke Swarm
 for ((i=1; i<${#MASTER_IPS[@]}; i++))
 do
-  sshpass -p "$SSH_PASSWORD" ssh ubuntu@${MASTER_IPS[$i]} "docker swarm join --token $JOIN_TOKEN ${MASTER_IPS[0]}:2377"
+  join_swarm "${MASTER_IPS[$i]}" "$MANAGER_JOIN_TOKEN"
 done
 
-# Join worker nodes to the Swarm
+# Bergabungkan node worker ke Swarm
 for ((i=0; i<${#WORKER_IPS[@]}; i++))
 do
-  sshpass -p "$SSH_PASSWORD" ssh ubuntu@${WORKER_IPS[$i]} "docker swarm join --token $JOIN_TOKEN ${MASTER_IPS[0]}:2377"
+  join_swarm "${WORKER_IPS[$i]}" "$WORKER_JOIN_TOKEN"
 done
+
+echo "Swarm initialization and node join process completed."
