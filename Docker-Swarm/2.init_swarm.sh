80c737eae57c78071f776e66d0fd57816699b6d9
Dani Maulana Ferdiansyah <47852543+danimaulana@users.noreply.github.com>
2024-05-30 02:46:14 +0700

    Update 2.init_swarm.sh

--- Docker-Swarm/2.init_swarm.sh
+++ Docker-Swarm/2.init_swarm.sh
@@ -1,1 +1,26 @@
+#!/bin/bash
 
+# List of IP addresses for master and worker nodes
+MASTER_IPS=("192.168.122.61" "192.168.122.62" "192.168.122.63")
+WORKER_IPS=("192.168.122.64" "192.168.122.65")
+
+# SSH password for authentication
+SSH_PASSWORD="ubuntu"
+
+# Initialize Swarm on the first master node
+docker swarm init --advertise-addr ${MASTER_IPS[0]}
+
+# Get token for joining the Swarm as additional master nodes
+JOIN_TOKEN=$(docker swarm join-token -q worker)
+
+# Join additional master nodes to the Swarm
+for ((i=1; i<${#MASTER_IPS[@]}; i++))
+do
+  sshpass -p "$SSH_PASSWORD" ssh ubuntu@${MASTER_IPS[$i]} "docker swarm join --token $JOIN_TOKEN ${MASTER_IPS[0]}:2377"
+done
+
+# Join worker nodes to the Swarm
+for ((i=0; i<${#WORKER_IPS[@]}; i++))
+do
+  sshpass -p "$SSH_PASSWORD" ssh ubuntu@${WORKER_IPS[$i]} "docker swarm join --token $JOIN_TOKEN ${MASTER_IPS[0]}:2377"
+done
